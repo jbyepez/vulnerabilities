1e1d70fab1464cdfaa56004f348ee3a314fc08ce
John Bryan Yepez <john.yepez@pragma.com.co>
2022-06-17 17:35:58 -0500

    initial commit

---
+++ src/main/java/com/test/vulnerabilities/VulnerabilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.test.vulnerabilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnerabilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnerabilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/vulnerabilities/VulnerabilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.vulnerabilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnerabilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
